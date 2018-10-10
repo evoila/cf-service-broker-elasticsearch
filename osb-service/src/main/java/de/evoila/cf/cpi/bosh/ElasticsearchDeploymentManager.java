1ad2678724ed05eef7630ca5e6a1f355422626d3
Michael Hahn <mail@hahn-michael.com>
2018-10-10 17:16:45 +0200

    feat(DeploymentManager): manipulate provides and consumes in manifest
    Now the provides and consumes properties of the plan will be taken into account when
    manipulating the manifest.

--- osb-service/src/main/java/de/evoila/cf/cpi/bosh/ElasticsearchDeploymentManager.java
+++ osb-service/src/main/java/de/evoila/cf/cpi/bosh/ElasticsearchDeploymentManager.java
@@ -1,12 +1,12 @@
 package de.evoila.cf.cpi.bosh;
 
 import de.evoila.cf.broker.bean.BoshProperties;
-import de.evoila.cf.broker.model.Plan;
-import de.evoila.cf.broker.model.ServiceInstance;
-import de.evoila.cf.broker.model.User;
+import de.evoila.cf.broker.model.*;
 import de.evoila.cf.broker.util.MapUtils;
 import de.evoila.cf.cpi.bosh.deployment.DeploymentManager;
+import de.evoila.cf.cpi.bosh.deployment.manifest.InstanceGroup;
 import de.evoila.cf.cpi.bosh.deployment.manifest.Manifest;
+import de.evoila.cf.cpi.bosh.deployment.manifest.instanceGroup.JobV2;
 import org.assertj.core.util.Sets;
 import org.springframework.core.env.Environment;
 
@@ -114,6 +114,67 @@ public class ElasticsearchDeploymentManager extends DeploymentManager {
             }
         }
         return 1;
+    }
+
+    @Override
+    protected void updateInstanceGroupConfiguration(Manifest manifest, Plan plan) {
+        super.updateInstanceGroupConfiguration(manifest, plan);
+
+        Metadata metadata = plan.getMetadata();
+
+        for(InstanceGroup instanceGroup : manifest.getInstanceGroups()) {
+            if(metadata != null) {
+                updateProvidesAndConsumes(instanceGroup, metadata);
+
+
+                InstanceGroupConfig instanceGroupConfig = metadata.getInstanceGroupConfig().stream()
+                        .filter(i -> i.getName() != null && i.getName().equals(instanceGroup.getName()))
+                        .findFirst()
+                        .orElse(null);
+
+                if(metadata.getInstanceGroupConfig() != null && instanceGroupConfig != null) {
+                    updateProvidesAndConsumes(instanceGroup, instanceGroupConfig);
+                }
+            }
+        }
+    }
+
+    private void updateProvidesAndConsumes(InstanceGroup instanceGroup, InstanceGroupConfig instanceGroupConfig) {
+        if(instanceGroupConfig instanceof CustomInstanceGroupConfig) {
+            CustomInstanceGroupConfig customInstanceGroupConfig = (CustomInstanceGroupConfig) instanceGroupConfig;
+
+            if (customInstanceGroupConfig.getProvides() != null) {
+                instanceGroup.getJobs().stream()
+                        .filter(i -> i.getName() != null && i.getName().equals("elasticsearch"))
+                        .findFirst()
+                        .get()
+                        .setProvides(customInstanceGroupConfig.getProvides());
+
+                // change consumes of smoke-test
+                HashMap<String, Object> consumesMap = new HashMap<>();
+
+                customInstanceGroupConfig.getProvides().entrySet().forEach(m -> {
+                    HashMap<String, Object> fromMap = new HashMap<>();
+
+                    if (m.getValue() instanceof HashMap && m.getValue() != null) {
+                        HashMap<String, Object> asMap = (HashMap<String, Object>) m.getValue();
+                        fromMap.put("from", asMap.get("as"));
+
+                        consumesMap.put(m.getKey(), fromMap);
+                    }
+                });
+
+                JobV2 smokeTests = instanceGroup.getJobs().stream()
+                        .filter(i -> i.getName() != null && i.getName().equals("smoke-tests"))
+                        .findFirst()
+                        .orElse(null);
+
+                smokeTests.setConsumes(consumesMap);
+            }
+
+            if (customInstanceGroupConfig.getConsumes() != null)
+                instanceGroup.getJobs().forEach(m -> m.setConsumes(customInstanceGroupConfig.getConsumes()));
+        }
     }
 
     private String generatePassword() {
