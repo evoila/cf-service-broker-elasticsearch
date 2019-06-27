f9e67e66ce28e8b8d87d1460234017c7687ed82d
Michael Hahn <mail@hahn-michael.com>
2019-06-27 13:54:09 +0200

    feat: add CredentialConstants for elasticsearch built-in user
    Added the class CredentialConstants to list username constants of built-in
    elasticsearch users. Feel free to add new credential related constants.

---
+++ osb-service/src/main/java/de/evoila/cf/broker/service/custom/constants/CredentialConstants.java
@@ -0,0 +1,45 @@
+package de.evoila.cf.broker.service.custom.constants;
+
+/**
+ * This class contains constants like built-in user names and other
+ * credential related constants.
+ *
+ * @author Michael Hahn
+ */
+public class CredentialConstants {
+
+    /**
+     * A built-in superuser.
+     */
+    public static String SUPER_ADMIN = "elastic";
+
+    /**
+     * The user Kibana uses to connect and communicate with Elasticsearch.
+     */
+    public static String KIBANA_USER = "kibana";
+
+    /**
+     * The user Logstash uses when storing monitoring information in Elasticsearch.
+     */
+    public static String LOGSTASH_USER = "logstash_system";
+
+    /**
+     * The user the Beats use when storing monitoring information in Elasticsearch.
+     */
+    public static String BEATS_USER = "beats_system";
+
+    /**
+     * The user the APM server uses when storing monitoring information in Elasticsearch.
+     */
+    public static String APM_USER = "apm_system";
+
+    /**
+     * The user Metricbeat uses when collecting and storing monitoring information in Elasticsearch.
+     */
+    public static String REMOTE_MONITORING_USER = "remote_monitoring_user";
+
+    /**
+     * The user the drain monitor uses.
+     */
+    public static String DRAIN_MONITOR_USER = "drain_monitor";
+}
